7f170041e00c83cf74495397f9ddfc188ca3d2fb
Shunqin.Chen <kentchensq@hotmail.com>
2019-02-27 11:50:03 +0800

    feat: try new version of spring boot - try 2.x spring-boot

---
+++ spring-boot/spring-boot-simple-2.0/src/main/java/lol/kent/practice/spring/Application.java
@@ -0,0 +1,15 @@
+package lol.kent.practice.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+* @author: Kent
+* */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
