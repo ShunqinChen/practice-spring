30d60de30abc7e8cb81e9c53ef0277133e366e74
Shunqin.Chen <kentchensq@hotmail.com>
2019-03-05 15:19:55 +0800

    feat: add ali-p3c-pmd support

---
+++ spring-boot-2.x/spring-wechat/src/main/java/lol/kent/practice/wechat/Application.java
@@ -0,0 +1,16 @@
+package lol.kent.practice.wechat;
+
+import lol.kent.practice.core.framework.spring.AbstractApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author Shunqin.Chen
+ */
+public class Application extends AbstractApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ spring-boot-2.x/spring-wechat/src/test/java/lol/kent/practice/wechat/springwechat/ApplicationTests.java
@@ -0,0 +1,16 @@
+package lol.kent.practice.wechat.springwechat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
