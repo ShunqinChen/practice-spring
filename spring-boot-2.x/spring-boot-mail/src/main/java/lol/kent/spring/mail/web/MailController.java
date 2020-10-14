0b359915e9d981c137cdf968f2575789436a9c74
Shunqin.Chen <kentchensq@gmail.com>
2020-10-14 17:30:28 +0800

    feat: 邮件发送测试

---
+++ spring-boot-2.x/spring-boot-mail/src/main/java/lol/kent/spring/mail/web/MailController.java
@@ -0,0 +1,60 @@
+package lol.kent.spring.mail.web;
+
+/**
+ * <pre>
+ *    类描述:
+ * </pre>
+ * <p>
+ * Copyright: Copyright (c) 2020年10月14日 16:40
+ * <p>
+ * Company: Luoke101.com
+ * <p>
+ *
+ * @author Shunqin.Chen
+ * @version 1.0.0
+ */
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/mail")
+public class MailController {
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @PostMapping
+    public void send() throws MessagingException {
+        String to = "4855046@qq.com";
+        String subject = "Springboot 发送简单文本邮件";
+        String content = "<h1>第一封 Springboot 简单文本邮件</h1>";
+        this.sendHtmlMail(to, subject, content);
+
+    }
+
+    public void sendHtmlMail(String to, String subject, String content) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
+        messageHelper.setFrom(from);
+        messageHelper.setTo(to);
+        messageHelper.setSubject(subject);
+        // true 为 HTML 邮件
+        messageHelper.setText(content, true);
+        mailSender.send(message);
+        log.info("【HTML 邮件】成功发送！to={}", to);
+    }
+
+}
