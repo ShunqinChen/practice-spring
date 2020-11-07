39f5cfb69c22ad2b4cac866cfaf483cce19bbb63
Shunqin.Chen <kentchensq@gmail.com>
2020-11-07 13:50:02 +0800

    feat: mail简单测试

---
+++ spring-boot-2.x/spring-boot-mail/src/main/java/lol/kent/spring/mail/SpringBootMailApplication.java
@@ -0,0 +1,13 @@
+package lol.kent.spring.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMailApplication.class, args);
+    }
+
+}
