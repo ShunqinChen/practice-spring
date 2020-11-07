d027d31fd3a3e9ab09471d3f80df81edfea311da
Shunqin.Chen <kentchensq@gmail.com>
2020-11-07 16:34:24 +0800

    feat: 完成基于consul的服务发现及配置,并测试了自定义Notifier

---
+++ spring-cloud/admin-consul-client/src/main/java/lol/kent/practice/Bootstrap.java
@@ -0,0 +1,15 @@
+package lol.kent.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstrap.class, args);
+    }
+
+}
