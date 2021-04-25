cb5f230c6bba1f3ce6330249f6558d13b1b15415
kentchensq@gmail.com <rX14481946>
2021-04-25 14:54:04 +0800

    feat: zipkin server with springboot test

---
+++ spring-cloud/sleuth-zipkin-server/src/main/java/lol/kent/practice/sleuth/server/Bootstrap.java
@@ -0,0 +1,29 @@
+package lol.kent.practice.sleuth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
+
+/**
+ * <pre>
+ *    类描述:
+ * </pre>
+ * <p>
+ * Copyright: Copyright (c) 2021年04月23日 11:50
+ * <p>
+ * Company: Luoke101.com
+ * <p>
+ *
+ * @author Shunqin.Chen
+ * @version 1.0.0
+ */
+@EnableZipkinServer
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstrap.class, args);
+    }
+
+}
